e06df7757409b8c1ce47d13f083099029dd30760
Olga Stan <oljstan@gmail.com>
2021-03-14 17:50:25 +0200

    html, css, js files optimization

--- index.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('#burger').addEventListener('click', function() {
-    document.querySelector('#menu').classList.toggle('is-active');
-    });
-});
